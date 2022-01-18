b40823d6d11f0e85482c864145dac83a9c811c35
Joro <joro.georgiev@gmail.com>
2022-01-18 12:20:48 +0200

    ok

--- src/hooks/useAnimations.js
+++ src/hooks/useAnimations.js
@@ -13,11 +13,12 @@ export function useTextAnimations() {
         opacity: 0,
         yPercent: 50,
         duration: 1,
+        delay: 0.2,
 
         ease: "power2.out",
         scrollTrigger: {
           trigger: item,
-          start: "20px bottom",
+          start: "top bottom",
           toggleActions: "play none none reverse",
           id: "textAnimation",
         },
